8e003dd0b0d9e1ec72a0d9a8023d4f995dc9e2df
Alessio <98657126+alessiomartini@users.noreply.github.com>
2024-02-25 16:58:41 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,6 +9,6 @@ if (!empty($_POST)) {
     $msg = wordwrap($msg,70);
 
     // send email
-    mail("alessio.martini@issgreppi.it","alessiomartini.ddns.net",$msg);
+    mail("alemarti.2001@gmail.com","alessiomartini.ddns.net",$msg);
 }
 ?>
